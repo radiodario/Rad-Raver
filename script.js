33573716eab62200e38df0ccd03a63e262ea060e
radiodario <dario.villanueva@gmail.com>
2015-02-09 13:18:07 +0000

    indent using spaces

--- script.js
+++ script.js
@@ -1,196 +1,196 @@
 ;(function() {
 
-	var	$ 			= document,
-		canvas 		= $.getElementsByTagName('canvas')[0],
-		ctx 		= canvas.getContext('2d'),
-		worker 		= new Worker('./worker.js'),
-		resolution 	= 4, // The pixel size/step
-		tick;
+  var $       = document,
+    canvas    = $.getElementsByTagName('canvas')[0],
+    ctx     = canvas.getContext('2d'),
+    worker    = new Worker('./worker.js'),
+    resolution  = 4, // The pixel size/step
+    tick;
 
-	ctx.webkitImageSmoothingEnabled = false;
+  ctx.webkitImageSmoothingEnabled = false;
 
-	function resize() {
-		// @todo Write code to calculate optimal pixel size/step
-		canvas.width = window.innerWidth;
-		canvas.height = window.innerHeight;
-	}
+  function resize() {
+    // @todo Write code to calculate optimal pixel size/step
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+  }
 
-	resize(), window.addEventListener('resize', resize);
+  resize(), window.addEventListener('resize', resize);
 
-	worker.onmessage = function(e) {
-		
-		switch(e.data.obj) {
-			case 'nearRoad':
-				nearRoad.data = e.data.payload;
-				break;
-			
-			case 'farRoad':
-				farRoad.data = e.data.payload;
-				break;
-			
-			case 'car':
-				car.data = e.data.payload;
-				break;
+  worker.onmessage = function(e) {
 
-			case 'sky':
-				sky.data = e.data.payload;
-				break;
-			
-			case 'hud':
-				hud.data = e.data.payload;
-				break;
+    switch(e.data.obj) {
+      case 'nearRoad':
+        nearRoad.data = e.data.payload;
+        break;
 
-			default:
-				// The only reason I'm using throw is because this project needs to be completed asap, so no soft logging, just hard and cruel errors
-				throw "Main didn't understand your request with obj value of '" + e.data.obj + "'";
-		}
+      case 'farRoad':
+        farRoad.data = e.data.payload;
+        break;
 
-	};
+      case 'car':
+        car.data = e.data.payload;
+        break;
 
-	var nearRoad = function() {
-		this.data = {};
-		var bias = false;
-		var pieces = 40;
+      case 'sky':
+        sky.data = e.data.payload;
+        break;
 
-		this.draw = function(canvas, ctx) {
-			if(tick % 2 === 0) bias = !bias;
+      case 'hud':
+        hud.data = e.data.payload;
+        break;
 
-			for(var i=0; i<pieces; i++) {
-				if(bias) {
-					if(i % 2 === 0) ctx.fillStyle = '#ff0000';
-					else ctx.fillStyle = '#fff';
-				} else {
-					if(i % 2 === 0) ctx.fillStyle = '#fff';
-					else ctx.fillStyle = '#ff0000';
-				}
+      default:
+        // The only reason I'm using throw is because this project needs to be completed asap, so no soft logging, just hard and cruel errors
+        throw "Main didn't understand your request with obj value of '" + e.data.obj + "'";
+    }
 
-				ctx.fillRect(canvas.width/6 - (20*i)/2 + this.data.x,
-					canvas.height/2 + (canvas.height/(pieces*2) * i),
-					canvas.width/1.5 + (20*i),
-					canvas.height/pieces
-				);
+  };
 
-				ctx.fillStyle = 'gray';
+  var nearRoad = function() {
+    this.data = {};
+    var bias = false;
+    var pieces = 40;
 
-				ctx.fillRect(canvas.width/6 + 10 - (20*i) /2  + this.data.x,
-					canvas.height/2 + (canvas.height/(pieces*2) * i),
-					canvas.width/1.5 + (20*i) - 20,
-					canvas.height/pieces
-				);
-			}
+    this.draw = function(canvas, ctx) {
+      if(tick % 2 === 0) bias = !bias;
 
-		};
+      for(var i=0; i<pieces; i++) {
+        if(bias) {
+          if(i % 2 === 0) ctx.fillStyle = '#ff0000';
+          else ctx.fillStyle = '#fff';
+        } else {
+          if(i % 2 === 0) ctx.fillStyle = '#fff';
+          else ctx.fillStyle = '#ff0000';
+        }
 
-		this.update = function() {
-			worker.postMessage({obj: 'nearRoad'});
-		};
+        ctx.fillRect(canvas.width/6 - (20*i)/2 + this.data.x,
+          canvas.height/2 + (canvas.height/(pieces*2) * i),
+          canvas.width/1.5 + (20*i),
+          canvas.height/pieces
+        );
 
-	};
+        ctx.fillStyle = 'gray';
 
-	var farRoad = function() {
-		this.data = {};
+        ctx.fillRect(canvas.width/6 + 10 - (20*i) /2  + this.data.x,
+          canvas.height/2 + (canvas.height/(pieces*2) * i),
+          canvas.width/1.5 + (20*i) - 20,
+          canvas.height/pieces
+        );
+      }
 
-		this.draw = function(canvas, ctx) {
+    };
 
-		};
+    this.update = function() {
+      worker.postMessage({obj: 'nearRoad'});
+    };
 
-		this.update = function() {
-			worker.postMessage({obj: 'farRoad'});
-		};
+  };
 
-	};
+  var farRoad = function() {
+    this.data = {};
 
-	var car = function() {
-		this.data = {};
+    this.draw = function(canvas, ctx) {
 
-		this.draw = function(canvas, ctx) {
+    };
 
-		};
+    this.update = function() {
+      worker.postMessage({obj: 'farRoad'});
+    };
 
-		this.update = function() {
-			worker.postMessage({obj: 'car'});
-		};
+  };
 
-	};
+  var car = function() {
+    this.data = {};
 
-	var sky = function() {
-		this.data = {};
+    this.draw = function(canvas, ctx) {
+
+    };
+
+    this.update = function() {
+      worker.postMessage({obj: 'car'});
+    };
+
+  };
+
+  var sky = function() {
+    this.data = {};
 
 
-		var image = new Image();
-		image.src = './lib/img/sky.png';
-		this.draw = function(canvas, ctx) {
+    var image = new Image();
+    image.src = './lib/img/sky.png';
+    this.draw = function(canvas, ctx) {
 
-			// i = -1 because we've gotta cover the sky moving left as well
-			for(var i=-1; i < Math.ceil(canvas.width / image.width); i++) {
-				for(var j=0; j < Math.ceil(canvas.height / image.height); j++) {
-					ctx.drawImage(image, (image.width * i) + this.data.x, image.height * j);
-				}
-			}
-		
-		};
+      // i = -1 because we've gotta cover the sky moving left as well
+      for(var i=-1; i < Math.ceil(canvas.width / image.width); i++) {
+        for(var j=0; j < Math.ceil(canvas.height / image.height); j++) {
+          ctx.drawImage(image, (image.width * i) + this.data.x, image.height * j);
+        }
+      }
 
-		this.update = function() {
-			worker.postMessage({obj: 'sky'});
-		};
+    };
 
-	};
+    this.update = function() {
+      worker.postMessage({obj: 'sky'});
+    };
 
-	var ground = function() {
-		this.data = {};
+  };
 
-		this.draw = function(canvas, ctx) {
+  var ground = function() {
+    this.data = {};
 
-			ctx.fillStyle = '#000';
-			ctx.fillRect(0, canvas.height/2, canvas.width, canvas.height/2);
-		
-		};
+    this.draw = function(canvas, ctx) {
 
-		this.update = function() {
-			
-		};
+      ctx.fillStyle = '#000';
+      ctx.fillRect(0, canvas.height/2, canvas.width, canvas.height/2);
 
-	};
+    };
 
-	var hud = function() {
-		this.data = {};
+    this.update = function() {
 
-		this.draw = function(canvas, ctx) {
+    };
 
-		};
+  };
 
-		this.update = function() {
-			worker.postMessage({obj: 'hud'});
-		};
+  var hud = function() {
+    this.data = {};
 
-	};
+    this.draw = function(canvas, ctx) {
 
-	nearRoad = new nearRoad();
-	farRoad = new farRoad();
-	car = new car();
-	sky = new sky();
-	ground = new ground();
-	hud = new hud();
+    };
 
-	function loop() {
-		tick = Math.floor(new Date().getTime() / 1000);
+    this.update = function() {
+      worker.postMessage({obj: 'hud'});
+    };
 
-		sky.update();
-		farRoad.update();
-		nearRoad.update();
-		car.update();
-		hud.update();
+  };
 
-		requestAnimationFrame(loop);
+  nearRoad = new nearRoad();
+  farRoad = new farRoad();
+  car = new car();
+  sky = new sky();
+  ground = new ground();
+  hud = new hud();
 
-		sky.draw(canvas, ctx);
-		ground.draw(canvas, ctx);
-		farRoad.draw(canvas, ctx);
-		nearRoad.draw(canvas, ctx);
-		car.draw(canvas, ctx);
-		hud.draw(canvas, ctx);
-	}
+  function loop() {
+    tick = Math.floor(new Date().getTime() / 1000);
 
-	requestAnimationFrame(loop);
+    sky.update();
+    farRoad.update();
+    nearRoad.update();
+    car.update();
+    hud.update();
+
+    requestAnimationFrame(loop);
+
+    sky.draw(canvas, ctx);
+    ground.draw(canvas, ctx);
+    farRoad.draw(canvas, ctx);
+    nearRoad.draw(canvas, ctx);
+    car.draw(canvas, ctx);
+    hud.draw(canvas, ctx);
+  }
+
+  requestAnimationFrame(loop);
 
 })();
--- worker.js
+++ worker.js
@@ -1,89 +1,89 @@
 var data = {
-	nearRoad: {
+  nearRoad: {
 
-	},
-	farRoad: {
+  },
+  farRoad: {
 
-	},
-	car: {
-		x: 0,
-		y: 0
-	},
-	sky: {
-		x: 0
-	},
-	hud: {
+  },
+  car: {
+    x: 0,
+    y: 0
+  },
+  sky: {
+    x: 0
+  },
+  hud: {
 
-	}
+  }
 },
 tick;
 
 function updateNearRoad() {
-	var result = data.nearRoad;
+  var result = data.nearRoad;
 
-	// @todo Remove this test code
-	result.x = Math.cos(tick * 4) * 40;
+  // @todo Remove this test code
+  result.x = Math.cos(tick * 4) * 40;
 
-	postMessage({obj: 'nearRoad', payload: result});
-	data.nearRoad = result;
+  postMessage({obj: 'nearRoad', payload: result});
+  data.nearRoad = result;
 
-	postMessage({obj: 'nearRoad', payload: result});
+  postMessage({obj: 'nearRoad', payload: result});
 }
 
 function updateFarRoad() {
-	var result = data.farRoad;
+  var result = data.farRoad;
 
-	postMessage({obj: 'farRoad', payload: result});
+  postMessage({obj: 'farRoad', payload: result});
 }
 
 function updateCar() {
-	var result = data.car;
+  var result = data.car;
 
-	postMessage({obj: 'car', payload: result});
+  postMessage({obj: 'car', payload: result});
 }
 
 function updateSky() {
-	var result = data.sky;
+  var result = data.sky;
 
-	// @todo Remove this test code
-	result.x = Math.cos(tick * 5) * 40;
+  // @todo Remove this test code
+  result.x = Math.cos(tick * 5) * 40;
 
-	postMessage({obj: 'sky', payload: result});
-	data.sky = result;
+  postMessage({obj: 'sky', payload: result});
+  data.sky = result;
 }
 
 function updateHud() {
-	var result = data.hud;
+  var result = data.hud;
 
-	postMessage({obj: 'hud', payload: result});
+  postMessage({obj: 'hud', payload: result});
 }
 
 onmessage = function(e) {
-	tick = new Date().getTime() / 1000;
-	switch(e.data.obj) {
-		case 'nearRoad':
-			updateNearRoad();
-			break;
-		
-		case 'farRoad':
-			updateFarRoad();
-			break;
-		
-		case 'car':
-			updateCar();
-			break;
+  tick = new Date().getTime() / 1000;
+  switch(e.data.obj) {
+    case 'nearRoad':
+      updateNearRoad();
+      break;
 
-		case 'sky':
-			updateSky();
-			break;
-		
-		case 'hud':
-			updateHud();
-			break;
+    case 'farRoad':
+      updateFarRoad();
+      break;
 
-		default:
-			// The only reason I'm using throw is because this project needs to be completed asap, so no soft logging, just hard and cruel errors
-			throw "Worker didn't understand your request with obj value of '" + e.data.obj + "'";
-	}
+    case 'car':
+      updateCar();
+      break;
+
+    case 'sky':
+      updateSky();
+      break;
+
+    case 'hud':
+      updateHud();
+      break;
+
+    default:
+      // The only reason I'm using throw is because this project needs to be completed asap, so no soft logging, just hard and cruel errors
+      throw "Worker didn't understand your request with obj value of '" + e.data.obj + "'";
+  }
 
 };
